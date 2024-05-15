092aaf31bce54ada092411c1d3d0b96c9978c240
Damon <654894017@qq.com>
2024-05-15 21:26:23 +0800

    Update Aggregate.java

--- src/main/java/com/damon/object_trace/Aggregate.java
+++ src/main/java/com/damon/object_trace/Aggregate.java
@@ -3,6 +3,7 @@ package com.damon.object_trace;
 
 import com.damon.object_trace.comparator.DeepComparator;
 import com.damon.object_trace.copier.DeepCopier;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 public class Aggregate<R extends Versionable> {
     private final int NEW_VERSION = 0;
@@ -25,7 +26,9 @@ public class Aggregate<R extends Versionable> {
      * @return true if the aggregate is changed, false if the aggregate is unchanged.
      */
     public boolean isChanged() {
-        return !deepComparator.isDeepEquals(root, snapshot);
+//        Boolean result= Objects.deepEquals(root, snapshot);
+///        return !deepComparator.isDeepEquals(root, snapshot);
+        return !EqualsBuilder.reflectionEquals(root, snapshot, false);
     }
 
 //    public boolean isNew() {

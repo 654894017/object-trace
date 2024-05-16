7ba41a7632d5587803226ccff860c2df30be94bb
231855 <231855@nd>
2024-05-17 01:21:42 +0800

    1.去除基于commons  的diff比较工具类实现

--- src/main/java/com/damon/object_trace/comparator/DeepComparator.java
+++ src/main/java/com/damon/object_trace/comparator/DeepComparator.java
@@ -3,5 +3,5 @@ package com.damon.object_trace.comparator;
 import com.damon.object_trace.Versionable;
 
 public interface DeepComparator {
-    <T extends Versionable> boolean isDeepEquals(T a, T b);
+ //   <T extends Versionable> boolean isDeepEquals(T a, T b);
 }
--- src/main/java/com/damon/object_trace/comparator/DiffDeepComparator.java
+++ src/main/java/com/damon/object_trace/comparator/DiffDeepComparator.java
@@ -4,9 +4,9 @@ import com.damon.object_trace.Versionable;
 import org.apache.commons.lang3.builder.DiffResult;
 
 public class DiffDeepComparator implements DeepComparator {
-    @Override
-    public <T extends Versionable> boolean isDeepEquals(T a, T b) {
-        DiffResult<T> result = a.diff(b);
-        return result.getDiffs().size() == 0;
-    }
+//    @Override
+//    public <T extends Versionable> boolean isDeepEquals(T a, T b) {
+//        DiffResult<T> result = a.diff(b);
+//        return result.getDiffs().size() == 0;
+//    }
 }

ae428ff8263ef9267ae946d9308f81e149181a99
Damon <654894017@qq.com>
2024-05-16 10:09:16 +0800

    Update Versionable.java

--- src/main/java/com/damon/object_trace/Versionable.java
+++ src/main/java/com/damon/object_trace/Versionable.java
@@ -1,16 +1,11 @@
 package com.damon.object_trace;
 
-import org.apache.commons.lang3.builder.DiffResult;
-import org.apache.commons.lang3.builder.Diffable;
-import org.apache.commons.lang3.builder.ReflectionDiffBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
-public interface Versionable<T, K> extends Diffable<T>, ID<K> {
+public interface Versionable<T, K> extends ID<K> {
     Integer getVersion();
 
     void setVersion(Integer version);
-
-    default DiffResult<T> diff(T obj) {
-        return new ReflectionDiffBuilder(this, obj, ToStringStyle.SHORT_PREFIX_STYLE).build();
-    }
+//    @Override
+//    default DiffResult<T> diff(T obj) {
+//        return new ReflectionDiffBuilder(this, obj, ToStringStyle.SHORT_PREFIX_STYLE).build();
+//    }
 }
